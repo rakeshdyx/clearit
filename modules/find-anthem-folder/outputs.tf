972d751f724bd2d22e94b8f148630cad722aeef8
rakeshdyx <rakeshdyx@gmail.com>
2022-09-26 08:47:54 +0530

    cleared

--- modules/find-anthem-folder/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "folder" {
-  description = "The requested anthem folder."
-  value = local.layer == 2 ? data.google_active_folder.l2_folder[0] : data.google_active_folder.l1_folder
-}
--- modules/find-anthem-project/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "project" {
-  description = "The requested anthem project."
-  value = data.google_projects.anthem_project.projects[0]
-}
